a2ad17b5fcc2f87937f677a318489e4d77e273dd
xuliang482 <95121869@qq.com>
2018-04-04 01:38:44 +0900

    html image input add

--- config/app.php
+++ config/app.php
@@ -167,6 +167,9 @@ return [
         /*
          * Package Service Providers...
          */
+         Collective\Html\HtmlServiceProvider::class,
+         Intervention\Image\ImageServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
@@ -226,10 +229,10 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Form'      => 'Collective\Html\FormFacade',
-        'Html'      => 'Collective\Html\HtmlFacade',
-        'Image'     => 'Intervention\Image\Facades\Image',
-
+        'Form'      => Collective\Html\FormFacade::class,
+        'Html'      => Collective\Html\HtmlFacade::class,
+        'Image' => Intervention\Image\Facades\Image::class,
+        'Input' => Illuminate\Support\Facades\Input::class,
     ],
 
 ];
