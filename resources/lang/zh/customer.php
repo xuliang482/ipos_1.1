77402e0fdffe0788355b6862f196ef71c52e0682
eist3059 <liang.xu@konicaminolta.com>
2018-04-04 17:55:02 +0900

    lang customer

--- resources/lang/zh/customer.php
+++ resources/lang/zh/customer.php
@@ -14,18 +14,29 @@ return [
     */
 
     'id' => 'ID',
-    'avatar' => '头像',
+    'gender' => '性别',
     'name' => '名称',
-    'email' => '电子邮件',
+    'email' => '电子邮箱',
     'phone_number' => '电话',
-    'description' => '说明',
-    'active' => '状态',
+    'avatar' => '头像',
+    'zip' => '邮政编码',
+    'address' => '地址',
+    'province' => '省',
+    'city' => '市',
+    'district' => '区',
+    'comment' => '备注',
+    'company_name' => '公司名称',
+   
     'operation' => '操作',
-
     'submit'=> '提交',
     'create'=> '新客户',
     'export'=> '导出',
     'edit'	=> '编辑',
     'delete'=> '删除',
-    'update_customer' => 'Update Customer',
+    
+    'message_successful_label'=> '成功',
+    'message_successful_create'=> '您已成功添加客户',
+    'message_successful_delete'=> '您已成功删除客户',
+    'message_successful_edit'=> '您已成功更新客户',
+    
 ];
