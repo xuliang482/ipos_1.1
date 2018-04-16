a2803125022547d6d061ec725b066798c376033a
xuliang <xuliang482@gmail.com>
2018-04-16 17:15:48 +0900

    商品机能做成

---
+++ resources/lang/zh/item.php
@@ -0,0 +1,53 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'id' => 'ID',
+    'upc_ean_isbn' => '商品条码',
+    'name' => '商品名称',
+    'category' => '种类',
+    'cost_price' => '成本价格',
+    'selling_price' => '销售价格',
+    'quantity' => '数量',
+    'avatar' => '图像',
+    'description' => '说明',
+
+
+    'operation' => '操作',
+    'reset'=> '重置',
+    'submit'=> '提交',
+    'create'=> '新商品',
+    'import'=> 'Excel 导入',
+    'export'=> '导出',
+    'edit'	=> '编辑',
+    'delete'=> '删除',
+
+
+    'title' => '商品信息',
+    'back'=> '返回',
+    'list'=> '一览',
+
+    'message_successful_label'=> '成功',
+    'message_successful_create'=> '您已成功添加商品',
+    'message_successful_delete'=> '您已成功删除商品',
+    'message_successful_edit'=> '您已成功更新商品',
+
+    'validation_upc_ean_isbn_required' => '商品条码是必须输入项',
+    'validation_name_required' => '商品名称是必须输入项',
+    'validation_category_required' => '种类是必须输入项',
+    'validation_cost_price_required' => '成本价格是必须输入项',
+    'validation_selling_price_required' => '销售是必须输入项',
+    'validation_email_address' => '请输入一个有效的电子邮件地址',
+    
+];
