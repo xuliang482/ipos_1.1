c15fb996516c2503a816bbbc67ebde509f1e0935
xuliang <xuliang482@gmail.com>
2018-04-19 17:34:10 +0900

    bootstrap-table　添加。

--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -22,50 +22,35 @@
                          </div>
                     </div>
                 </div>
-                {!! Form::open(array('url' => 'customers', 'method' => 'PUT','class'=>'form-horizontal')) !!}
-                <div class="box-body table-responsive no-padding">
-                    <table class="table table-hover">
-                        <thead>
+                {!! Form::open(array('url' => 'customers/destroy', 'method' => 'DELETE','class'=>'form-horizontal')) !!}
+                <div class="box-body">
+                    <table id="table"
+                           data-toolbar="#toolbar"
+                           data-search="true"
+                           data-show-refresh="true"
+                           data-show-columns="true"
+                           data-show-export="true"
+                           data-minimum-count-columns="2"
+                           data-pagination="true"
+                           data-id-field="id"
+                           data-page-list="[10, 25, 50, 100, ALL]"
+                           data-classes="table table-hover"
+                           data-sort-name="id"
+       					   data-sort-order="desc"
+       					   data-click-to-select="true">
+                       <thead>
                             <tr>
-                             	<th width="10px;"><input type="checkbox" class="check"></th>
-                                <th>{{trans('customer.id')}}</th>
-                                <th>{{trans('customer.avatar')}}</th>
-                                <th>{{trans('customer.name')}}</th>
-                                <th>{{trans('customer.email')}}</th>
-                                <th>{{trans('customer.phone_number')}}</th>
-                                <th>{{trans('customer.comment')}}</th>
-                                <th>{{trans('customer.operation')}}</th>
+                             	<th data-field="state" data-checkbox="true" width="10px;"></th>
+                                <th data-field="id" 	data-sortable="true">{{trans('customer.id')}}</th>
+                                <th data-field="avatar" data-sortable="true">{{trans('customer.avatar')}}</th>
+                                <th data-field="name"   data-sortable="true">{{trans('customer.name')}}</th>
+                                <th data-field="email"  data-sortable="true">{{trans('customer.email')}}</th>
+                                <th data-field="phone_number" data-sortable="true">{{trans('customer.phone_number')}}</th>
+                                <th data-field="comment" data-sortable="true">{{trans('customer.comment')}}</th>
+                                <th data-field="operation" >{{trans('customer.operation')}}</th>
                             </tr>
-                        </thead>
-                        <tbody>
-                            @foreach($customer as $value)
-                            <tr>
-                            	<td><input type="checkbox" class="check" name="ids[]" id="customer_id_{{ $value->id }}" value="{{ $value->id }}"></td>
-                                <td>{{ $value->id }}</td>
-                                <td>{!! Html::image('/images/customers/' . $value->avatar, 'a picture', array('class' => 'thumb')) !!}</td>
-                                <td>{{ $value->name }}</td>
-                                <td>{{ $value->email }}</td>
-                                <td>{{ $value->phone_number }}</td>
-                                <td>{{ $value->description }}</td>
-                                <td>
-                                <a href="{{ URL::to('customers/' . $value->id . '/edit') }}">
-                                	<i class="fa fa-edit"></i>
-                                </a>
-                                </td>
-                            </tr>
-                            @endforeach
-                        </tbody>
+                    	</thead>
                     </table>
-            	</div>
-                <div class="box-footer">
-                    <div class="pull-left">
-                        <input type="checkbox" id="check-all">
-                        {!! Form::submit(trans('customer.delete'), array('class' => 'btn btn-default btn-flat')) !!}
-                    </div>
-
-                    <div class="pull-right">
-                        {{ $customer->links('partials.paginator') }}
-                    </div>
                 </div>
                 {!! Form::close() !!}
             </div>
@@ -82,15 +67,15 @@
         $(this).alert('close');
     });
 
-
     //iCheck for checkbox
-    $(document).ready(function(){
+     /*$(document).ready(function(){
         $('input').iCheck({
             checkboxClass: 'icheckbox_minimal-blue',
             increaseArea: '20%' // optional
         });
     });
 
+   
     $('#check-all').on('ifChecked', function(event) {
         $('.check').iCheck('check');
     });
@@ -108,8 +93,132 @@
         }
         $('#check-all').iCheck('update');
     });
+	*/
 
 
+
+    
+    
+    const $table = $('#table');
+    const $remove = $('#remove');
+    let selections = [];
+
+    var mydata = {!! $customer !!};
+    console.log(mydata);
+    $table.bootstrapTable({data: mydata});
+
+    function initTable() {
+      $table.bootstrapTable({
+        height: getHeight(),
+        columns: [
+           {
+              field: 'operate',
+              title: 'Item Operate',
+              align: 'center',
+              events: operateEvents,
+              formatter: operateFormatter
+            }
+        ]
+      });
+      // sometimes footer render error.
+      setTimeout(() => {
+        $table.bootstrapTable('resetView');
+      }, 200);
+      $table.on('check.bs.table uncheck.bs.table ' +
+                'check-all.bs.table uncheck-all.bs.table', () => {
+        $remove.prop('disabled', !$table.bootstrapTable('getSelections').length);
+
+        // save your data, here just save the current page
+        selections = getIdSelections();
+        // push or splice the selections if you want to save all data selections
+      });
+      $table.on('expand-row.bs.table', (e, index, row, $detail) => {
+        if (index % 2 == 1) {
+          $detail.html('Loading from ajax request...');
+          $.get('LICENSE', res => {
+            $detail.html(res.replace(/\n/g, '<br>'));
+          });
+        }
+      });
+      $table.on('all.bs.table', (e, name, args) => {
+        console.log(name, args);
+      });
+      $remove.click(() => {
+        const ids = getIdSelections();
+        $table.bootstrapTable('remove', {
+          field: 'id',
+          values: ids
+        });
+        $remove.prop('disabled', true);
+      });
+      $(window).resize(() => {
+        $table.bootstrapTable('resetView', {
+          height: getHeight()
+        });
+      });
+    }
+
+
+
+    function getIdSelections() {
+      return $.map($table.bootstrapTable('getSelections'), ({id}) => id);
+    }
+
+    function responseHandler(res) {
+      $.each(res.rows, (i, row) => {
+        row.state = $.inArray(row.id, selections) !== -1;
+      });
+      return res;
+    }
+
+    function detailFormatter(index, row) {
+      const html = [];
+      $.each(row, (key, value) => {
+        html.push(`<p><b>${key}:</b> ${value}</p>`);
+      });
+      return html.join('');
+    }
+
+    function operateFormatter(value, row, index) {
+      return [
+        '<a class="like" href="javascript:void(0)" title="Like">',
+        '<i class="fa fa-heart"></i>',
+        '</a>  ',
+        '<a class="remove" href="javascript:void(0)" title="Remove">',
+        '<i class="fa fa-remove"></i>',
+        '</a>'
+      ].join('');
+    }
+
+    window.operateEvents = {
+      'click .like': function (e, value, row, index) {
+        alert(`You click like action, row: ${JSON.stringify(row)}`);
+      },
+      'click .remove': function(e, value, {id}, index) {
+        $table.bootstrapTable('remove', {
+          field: 'id',
+          values: [id]
+        });
+      }
+    };
+
+    function totalPriceFormatter(data) {
+      let total = 0;
+      $.each(data, (i, {price}) => {
+        total += +(price.substring(1));
+      });
+      return `$${total}`;
+    }
+
+    function getHeight() {
+      return $(window).height() - $('h1').outerHeight(true);
+    }
+
+    $(() => {
+    	initTable();
+    })
+
+    
    </script>
 	
 @endsection
